18f380da719eef2bc0b1990ceb8f6d77b5b1cb09
Chethan S <cchethans14@gmail.com>
2022-11-12 08:59:33 +0530

    First-Commit

---
+++ src/main/java/com/spring/forms/forms_core/FormsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.spring.forms.forms_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/forms/forms_core/FormsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.forms.forms_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
